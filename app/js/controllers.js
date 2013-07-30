5b340c9894be2b8e8d872d7944b0a4f2dbfd163a
Nayan Ambali <nayan.ambali@gmail.com>
2013-07-30 19:50:33 +0530

    Adding AngularJs seed

---
+++ app/js/controllers.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('myApp.controllers', []).
+  controller('LoginController', ['$scope','$http','$location',function ($scope,$http,$location) {
+					  $scope.login = function(User) {
+					  		
+					  			var params = {'password':User.password ,'username':User.name};
+					  			var headers = {'X-Mifos-Platform-TenantId':'default','Content-Type':'application/json; charset=utf-8'};
+								var config = { 'method': 'POST', 'url': 'https://demo.openmf.org/mifosng-provider/api/v1/authentication', 'params': params ,'headers':headers};
+								// configuration object
+								$http(config)
+								.success(function(data, status, headers, config) {
+									if (data.authenticated) {
+										// succefull login
+										//alert('authenticated ' + data.username);
+										$location.path('/clientProfile').replace();;
+										//$scope.$apply();
+									}
+									else {
+									alert(data.username);
+									}
+								})
+								.error(function(data, status, headers, config) {
+								});
+					  };
+				}
+			])
+  .controller('MyCtrl2', [function() {
+
+  }])
+
+  .controller('MyCtrl1', [function() {
+
+  }]);
